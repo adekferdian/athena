b0fe154c605936954810fcd758a5f3dce2bbf76a
Herdiansyah <herdiansyah@edot.id>
2023-06-16 15:00:29 +0700

    bypass login

--- src/app/routing/Routes.tsx
+++ src/app/routing/Routes.tsx
@@ -7,12 +7,12 @@
 
 import React, {FC} from 'react'
 import {Redirect, Switch, Route} from 'react-router-dom'
-import {shallowEqual, useSelector} from 'react-redux'
+// import {shallowEqual, useSelector} from 'react-redux'
 import {MasterLayout} from '../../_metronic/layout/MasterLayout'
 import {PrivateRoutes} from './PrivateRoutes'
 import {Logout, AuthPage} from '../modules/auth'
 import {ErrorsPage} from '../modules/errors/ErrorsPage'
-import {RootState} from '../../setup'
+// import {RootState} from '../../setup'
 import AuthScreens from '../modules/auth/screens'
 import EmailVerification from '../modules/auth/components/EmailVerification'
 import EmailCustomerVerification from '../modules/auth/components/EmailCustomerVerification'
@@ -20,7 +20,8 @@ import VerificationSuccess from '../modules/auth/components/VerificationSuccess'
 import {CreateNewPassword} from '../modules/auth/components/CreateNewPassword'
 
 const Routes: FC = () => {
-  const isAuthorized = useSelector<RootState>(({auth}) => Boolean(auth.accessToken), shallowEqual)
+  const isAuthorized = true
+  // useSelector<RootState>(({auth}) => Boolean(auth.accessToken), shallowEqual)
 
   return (
     <Switch>
